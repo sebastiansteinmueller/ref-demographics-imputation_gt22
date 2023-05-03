2fc2f2f932f11181b18626bd90cf4c094a608497
Sebastian Steinmueller <sebastian.steinmueller@gmail.com>
2023-05-03 18:16:09 +0200

    init after ignore data

--- preparedata.R
+++ preparedata.R
@@ -3,10 +3,10 @@
 ############################################ preparedata.R ################################################
 
 #### Queries: UNHCR Statistics and Demographics Section, UNICEF
-#### Project: Demographic models end-2021
-#### Description: Data preparation of ASR 2021 REF + VDA demographic data for imputation model
+#### Project: Demographic models end-2022
+#### Description: Data preparation of ASR 2022 REF + OIP demographic data for imputation model
 
-##### I. Read data, packages etc ##### 
+##### I. Read data, packages etc #####
 
 ### packages
 library(tidyverse)
@@ -25,7 +25,7 @@ download.file(vdaUrl, vdaTemp,  method = "curl", mode="wb", quiet = FALSE)
 demvda <- read_csv(unz(vdaTemp, "demographics.csv"), skip = 14)
 unlink(vdaTemp)
 
-demvda <- demvda %>% 
+demvda <- demvda %>%
   mutate(popType = "VDA")
 
 
@@ -37,7 +37,7 @@ download.file(refUrl, refTemp,  method = "curl", mode="wb", quiet = FALSE)
 demref <- read_csv(unz(refTemp, "demographics.csv"), skip = 14)
 unlink(refTemp)
 
-demref <- demref %>% 
+demref <- demref %>%
   mutate(popType = "REF")
 
 
@@ -45,7 +45,7 @@ demref <- demref %>%
 dim(demref)
 dim(demvda)
 
-dem <- demref %>% 
+dem <- demref %>%
   full_join(demvda)
 
 dim(dem) # OK
@@ -71,17 +71,17 @@ rm(list=c("refTemp", "vdaTemp", "refUrl", "vdaUrl", "demref", "demvda"))
 
 
 
-##### II. Check demographic data and create variables for missingness structure ##### 
+##### II. Check demographic data and create variables for missingness structure #####
 
 
 ### first check of demographic data
 
 summary(dem) # x_unknown variables are empty, whereas x_other are populated
 glimpse(dem) # confirmed by DAS team: x_other are age unknowns
-              
+
 
 ### clean variable names in demographic dataset
-dem <- dem %>% 
+dem <- dem %>%
   rename(
     year = Year,
     origin_iso3 = 'Country of origin (ISO)',
@@ -101,7 +101,7 @@ dem <- dem %>%
     male_AgeUnknown = 'Male other',
     male = 'Male total',
     totalEndYear = 'Total'
-  ) %>% 
+  ) %>%
   select(
     year,
     origin_iso3,
@@ -122,18 +122,18 @@ dem <- dem %>%
     male_AgeUnknown,
     male,
     totalEndYear
-  ) %>% 
+  ) %>%
   mutate(origin_iso3 = replace_na(origin_iso3, "NAA")) # replace missing origin iso3 codes with NAA
-  
+
 
 ### check missing age and sex counts and totals in demographic dataset
 
 ## sex
-dem <- dem %>% 
+dem <- dem %>%
   mutate(
     knownSexSum = rowSums(select(.,female, male), na.rm = T),
     sexAge_unknown = totalEndYear-knownSexSum # count of sex and age unknown in this asylum/origin/popType combination
-  ) %>% 
+  ) %>%
   mutate(
     sexSum = rowSums(select(., female, male, sexAge_unknown), na.rm = T), # check of all possible counts for sex (female, male, unknown)
     sexDiff = totalEndYear - sexSum # should be 0 (all accounted for in female, male or unknown)
@@ -141,50 +141,50 @@ dem <- dem %>%
 
 summary(dem$sexDiff) # OK (all 0)
 
-dem <- dem %>% 
+dem <- dem %>%
   mutate(
     female_AgeKnown = rowSums(select(., female_0_4, female_5_11, female_12_17, female_18_59, female_60), na.rm = T)
-  ) %>% 
+  ) %>%
   mutate(
     femaleAgeSum = rowSums(select(., female_AgeKnown, female_AgeUnknown), na.rm = T),
     femaleAgeDiff = female - femaleAgeSum # should be 0 (all accounted for in age or unknown age)
-  ) %>% 
+  ) %>%
   mutate(
     male_AgeKnown = rowSums(select(., male_0_4, male_5_11, male_12_17, male_18_59, male_60), na.rm = T)
   ) %>%
   mutate(
     maleAgeSum = rowSums(select(., male_AgeKnown, male_AgeUnknown), na.rm = T),
     maleAgeDiff = male - maleAgeSum # should be 0 (all accounted for in age or unknown age)
-  ) 
+  )
 
 summary(dem$femaleAgeDiff) # OK
 summary(dem$maleAgeDiff) # OK
 
 
-dem <- dem %>% 
+dem <- dem %>%
   mutate(
     age_unknown = rowSums(select(., female_AgeUnknown, male_AgeUnknown), na.rm = T), # count of age unknown, sex known in this asylum/origin/popType combination
     sexAge_known = rowSums(select(., female_AgeKnown, male_AgeKnown), na.rm = T) # count of age and sex known in this asylum/origin/popType combination
   ) %>%
   mutate(
     knownUnknownSum = rowSums(select(., sexAge_known, age_unknown, sexAge_unknown))
-  ) %>% 
+  ) %>%
   mutate(
     knownUnknownDiff = totalEndYear - knownUnknownSum # should be all 0
-  ) 
+  )
 
 
 summary(dem$knownUnknownDiff) # OK (all 0)
 
 # delete check variables
 
-dem <- dem %>% 
-  select(-c(knownSexSum, sexSum, sexDiff, femaleAgeSum, femaleAgeDiff, maleAgeSum, maleAgeDiff, 
+dem <- dem %>%
+  select(-c(knownSexSum, sexSum, sexDiff, femaleAgeSum, femaleAgeDiff, maleAgeSum, maleAgeDiff,
             knownUnknownSum, knownUnknownDiff))
 
 
 
-##### III. Merge m49 and UNHCR country information files into dataset m49hcr ##### 
+##### III. Merge m49 and UNHCR country information files into dataset m49hcr #####
 
 
 ### merge m49 dataset with UNHCR region codes and clean variable names
@@ -196,33 +196,33 @@ unhcr_iso3_missingM49 <- unhcr_iso3[!(unhcr_iso3 %in% m49$`ISO-alpha3 Code`)] # 
 unhcr_iso3_missingCountries <- unhcr_iso3[!(unhcr_iso3 %in% countries$iso3)] # "XXA" "TIB", NAA (Stateless, Tibet, Unknown) are in unhcr demographic data, but not in UNHCR country file
 
 countries_iso3_missingM49 <- countries$iso3[!(countries$iso3 %in% m49$`ISO-alpha3 Code`)] # "TWN", "XKX" (Taiwan, Kosovo) are in unhcr demographic data, but not in m49
- 
+
 
 # Stateless and Tibet: keep structure including those two codes in imputed dataset
-# covariates and regions: use those for China for Tibet/Taiwan, Serbia for Kosovo. For XXA and NAA, set region to own "unknown" level, use means of covariates (distance and GDP) 
+# covariates and regions: use those for China for Tibet/Taiwan, Serbia for Kosovo. For XXA and NAA, set region to own "unknown" level, use means of covariates (distance and GDP)
 
 
 ## add missing origins / asylum countries to m49 dataset
-m49 <- m49 %>% 
+m49 <- m49 %>%
   rename(
     region="Region Name",
     subregion="Sub-region Name",
     country = "Country or Area",
     m49 = "M49 Code",
     iso3 = "ISO-alpha3 Code"
-  )  %>% 
+  )  %>%
   select(region, subregion, country, m49, iso3) %>%
   add_row(filter(., iso3 == "CHN") %>% mutate(iso3 = "TIB", country = "Tibet", m49 = 156)) %>%
-  add_row(filter(., iso3 == "CHN") %>% mutate(iso3 = "TWN", country = "Taiwan", m49 = 158)) %>% 
-  add_row(region = "Unknown", subregion = "Unknown", country = "Stateless", iso3 = "XXA", m49 = 997) %>% 
-  add_row(region = "Unknown", subregion = "Unknown", country = "Unknown", iso3 = "NAA", m49 = 998) %>% 
+  add_row(filter(., iso3 == "CHN") %>% mutate(iso3 = "TWN", country = "Taiwan", m49 = 158)) %>%
+  add_row(region = "Unknown", subregion = "Unknown", country = "Stateless", iso3 = "XXA", m49 = 997) %>%
+  add_row(region = "Unknown", subregion = "Unknown", country = "Unknown", iso3 = "NAA", m49 = 998) %>%
   add_row(filter(., iso3 == "SRB") %>% mutate(iso3 = "XKX", country = "Kosovo", m49 = 412))
 
 ## add missing origins / asylum countries to countries dataset
-countries <- countries %>% 
+countries <- countries %>%
   select(iso3, proGres_code, main_office_short, hcr_region, hcr_subregion) %>%
-  add_row(filter(., iso3 == "CHN") %>% mutate(iso3 = "TIB",  proGres_code = "TIB")) %>% 
-  add_row(main_office_short = "Unknown", hcr_region = "Unknown", hcr_subregion = "Unknown", iso3 = "XXA", proGres_code = "XXA") %>% 
+  add_row(filter(., iso3 == "CHN") %>% mutate(iso3 = "TIB",  proGres_code = "TIB")) %>%
+  add_row(main_office_short = "Unknown", hcr_region = "Unknown", hcr_subregion = "Unknown", iso3 = "XXA", proGres_code = "XXA") %>%
   add_row(main_office_short = "Unknown", hcr_region = "Unknown", hcr_subregion = "Unknown", iso3 = "NAA", proGres_code = "NAA")
 
 
@@ -230,7 +230,7 @@ countries <- countries %>%
 dim(m49)
 dim(countries)
 
-m49hcr <- m49 %>% 
+m49hcr <- m49 %>%
   left_join(countries, by = "iso3")
 
 dim(m49hcr) # OK
@@ -238,7 +238,7 @@ sum(duplicated(m49hcr$iso3)) # OK, no duplicates
 
 
 
-##### IV. Merge demographic dataset with m49hcr ##### 
+##### IV. Merge demographic dataset with m49hcr #####
 
 dim(dem)
 dim(m49hcr)
@@ -254,8 +254,8 @@ m49hcr_origin <- m49hcr %>%
 
 ## merge with demographics dataset
 
-dem <- dem %>% 
-  left_join(m49hcr_asylum, by = "asylum_iso3") %>% 
+dem <- dem %>%
+  left_join(m49hcr_asylum, by = "asylum_iso3") %>%
   left_join(m49hcr_origin, by = "origin_iso3")
 dim(dem) # OK
 
@@ -268,7 +268,7 @@ table(dem$origin_region, useNA = "ifany")
 # all OK
 
 
-##### V. Merge distance and GNI covariates to dataset ##### 
+##### V. Merge distance and GNI covariates to dataset #####
 
 ### checks: are all origin/asylum combinations from dem available in distance, neighbour and GNI datasets without missings?
 
@@ -278,70 +278,70 @@ pairs_iso3 <- unique(dem %>% unite("pairs_iso3", origin_iso3, asylum_iso3) %>% s
 ## check neighbours
 # are all origins in the neighbour matrix?
 neighbour_ori_iso3 <- unique(all.neighbors$country_iso3)
-check_neighbour_ori <- dem %>% 
+check_neighbour_ori <- dem %>%
   filter(!(origin_iso3 %in% neighbour_ori_iso3)) # OK (islands, stateless, unknown)
 
-neighbour_asy_iso3 <- unique(all.neighbors$neighbor_iso3) 
-check_neighbour_asy <- dem %>% 
+neighbour_asy_iso3 <- unique(all.neighbors$neighbor_iso3)
+check_neighbour_asy <- dem %>%
   filter(!(asylum_iso3 %in% neighbour_asy_iso3)) # OK (islands)
 
 
 ## check distance
 distance_pairs <- unique(unlist(distance_df_long %>% unite("pairs_iso3", orig_iso3, dest_iso3) %>% select(pairs_iso3)))
 
-check_distance <- pairs_iso3 %>% 
+check_distance <- pairs_iso3 %>%
   filter(!(pairs_iso3 %in% distance_pairs)) # OK (only stateless and unknown origin missing in distance matrix)
 
 ## check gni
 gni_pairs <- unique(unlist(gni_diff_long %>% unite("pairs_iso3", orig_iso3, dest_iso3) %>% select(pairs_iso3)))
 
-check_gni <- pairs_iso3 %>% 
+check_gni <- pairs_iso3 %>%
   filter(!(pairs_iso3 %in% gni_pairs)) # OK (only stateless and unknown origin missing in gni matrix)
 
 
 ### merge covariates to dem
 
 dim(dem)
-dem <- dem %>% 
-  unite("pairs_iso3", origin_iso3, asylum_iso3, remove = F) %>% 
+dem <- dem %>%
+  unite("pairs_iso3", origin_iso3, asylum_iso3, remove = F) %>%
   left_join(
     all.neighbors %>% # neighbours
-      unite("pairs_iso3", country_iso3, neighbor_iso3) %>% 
+      unite("pairs_iso3", country_iso3, neighbor_iso3) %>%
       mutate(neighbor = "Yes"),
     by = "pairs_iso3"
-  ) %>% 
+  ) %>%
   mutate(neighbor = replace_na(neighbor, "No")) %>%
   left_join( # distance
-    distance_df_long %>% 
+    distance_df_long %>%
       select(orig_iso3, dest_iso3, distance) %>%
       unite("pairs_iso3", orig_iso3, dest_iso3),
     by = "pairs_iso3"
-  ) %>% 
+  ) %>%
   left_join( # GNI difference and origin
-    gni_diff_long %>% 
+    gni_diff_long %>%
       select(orig_iso3, dest_iso3, orig_gni, gni_diff) %>%
       rename(gni_origin = orig_gni) %>%
       unite("pairs_iso3", orig_iso3, dest_iso3),
     by = "pairs_iso3"
-  )  %>% 
+  )  %>%
   left_join( # GNI by asylum
-    gni_diff_long %>% 
+    gni_diff_long %>%
       select(dest_iso3, dest_gni) %>%
       distinct() %>%
       rename(gni_asylum = dest_gni),
     by = c("asylum_iso3"="dest_iso3")
-  ) 
+  )
 dim(dem)
 
 ### for missing values of distance, GNI by origin and GNI difference (stateless and unknown origins), impute population-weighted mean of refugees/VDA with known origin by country of asylum
 
-dem <- dem %>% 
+dem <- dem %>%
   group_by(asylum_iso3) %>%
   mutate(distanceAsyMeanW = weighted.mean(distance, w=totalEndYear, na.rm=T),
          gni_originAsyMeanW = weighted.mean(gni_origin, w=totalEndYear, na.rm=T),
          gni_diffAsyMeanW = weighted.mean(gni_diff, w=totalEndYear, na.rm=T)
-        ) %>% 
-  ungroup() %>% 
+        ) %>%
+  ungroup() %>%
   mutate(distance = case_when(
            is.na(distance) | distance == 0 ~ distanceAsyMeanW,
            !(is.na(distance) | distance == 0 ) ~ distance
@@ -354,18 +354,18 @@ dem <- dem %>%
            is.na(gni_diff) ~ gni_diffAsyMeanW,
            !(is.na(gni_diff)) ~ gni_diff
          )
-  ) %>% 
+  ) %>%
   select(-c(distanceAsyMeanW, gni_originAsyMeanW, gni_diffAsyMeanW))
 
 dim(dem %>% filter(is.na(distance)|is.na(gni_asylum)|is.na(gni_origin)|is.na(gni_diff))) # OK (empty)
 
 
-##### VI. Create long dataset with one row per asylum/origin/poptype combination and missingness type ##### 
+##### VI. Create long dataset with one row per asylum/origin/poptype combination and missingness type #####
 
-dem_longMissing <- dem %>% 
+dem_longMissing <- dem %>%
   pivot_longer(cols = c(sexAge_known, age_unknown, sexAge_unknown), # pivot to get separate missing/non-missing parts of dataframe for fit and predictions
               names_to = "missing",
-              values_to = "total") %>% 
+              values_to = "total") %>%
   mutate(missing = case_when( # sensible values for missing variable
       missing == "sexAge_unknown" ~ "sexAge",
       missing == "age_unknown" ~ "age",
@@ -380,36 +380,36 @@ dem_longMissing <- dem %>%
               missing == "none" ~ .
             )
         )
-  ) %>% 
+  ) %>%
   mutate(female = case_when( # set sex variables to NA if sex unknown, to number of unknown age in sex category if age only unknown, to number of known age if age known
           missing == "sexAge" ~ NA_real_,
           missing == "age" ~ female_AgeUnknown,
           missing == "none" ~ female_AgeKnown
         )
-  ) %>% 
+  ) %>%
   mutate(male = case_when(
     missing == "sexAge" ~ NA_real_,
     missing == "age" ~ male_AgeUnknown,
     missing == "none" ~ male_AgeKnown
     )
-  ) %>% 
-  select(year, origin_iso3, origin_country, asylum_iso3, asylum_country, popType, missing, 
-         female_0_4:female_60, female, male_0_4:male_60, male, total, 
+  ) %>%
+  select(year, origin_iso3, origin_country, asylum_iso3, asylum_country, popType, missing,
+         female_0_4:female_60, female, male_0_4:male_60, male, total,
          origin_region, origin_subregion, origin_m49:origin_hcr_subregion,
-         asylum_region, asylum_subregion, asylum_m49:asylum_hcr_subregion, 
-         neighbor:gni_asylum) %>% 
+         asylum_region, asylum_subregion, asylum_m49:asylum_hcr_subregion,
+         neighbor:gni_asylum) %>%
   mutate(across(c(origin_iso3:missing, origin_region:asylum_hcr_subregion), factor)) %>%
   mutate(neighbor = factor(neighbor, levels = c("Yes", "No"))) %>%
   mutate(year = as.integer(year)) %>%
   group_by(asylum_iso3) %>%
   mutate(distanceAsyMeanW = weighted.mean(distance, w=total, na.rm=T)
-  ) %>% 
-  ungroup() %>% 
+  ) %>%
+  ungroup() %>%
   mutate(distance = case_when(
     distance == 0 ~ distanceAsyMeanW,
     !(distance == 0 ) ~ distance
   )
-  ) %>% 
+  ) %>%
   mutate(gniRatio = gni_asylum/gni_origin,
          logGniRatio = log(gniRatio),
          logDistance = log(distance)
@@ -419,8 +419,8 @@ dem_longMissing <- dem %>%
          adults = rowSums(select(., female_18_59, female_60,
                                  male_18_59, male_60))
   )
-  
-dem_longMissing <- dem_longMissing %>% 
+
+dem_longMissing <- dem_longMissing %>%
   mutate(asylum_sdgregion = case_when(
     asylum_subregion %in% c("Australia and New Zealand") ~ "Australia and New Zealand",
     asylum_subregion %in% c("Central Asia", "Southern Asia") ~ "Central and Southern Asia",
@@ -438,29 +438,29 @@ dem_longMissing <- dem_longMissing %>%
 
 
 
-##### VII. Final checks: internal consistency of dataset, totals and proportion of missingness ##### 
+##### VII. Final checks: internal consistency of dataset, totals and proportion of missingness #####
 
 ## internal consistency
 
 # for age and sex available: do age categories add up to count in sex category?
-t.ageSexCheck <- dem_longMissing %>% 
-  filter(missing == "none") %>% 
+t.ageSexCheck <- dem_longMissing %>%
+  filter(missing == "none") %>%
   mutate(female_AgeKnown = rowSums(select(., female_0_4, female_5_11, female_12_17, female_18_59, female_60), na.rm = T),
-         male_AgeKnown = rowSums(select(., male_0_4, male_5_11, male_12_17, male_18_59, male_60), na.rm = T)) %>% 
+         male_AgeKnown = rowSums(select(., male_0_4, male_5_11, male_12_17, male_18_59, male_60), na.rm = T)) %>%
   mutate(femaleAgeDiff = female - female_AgeKnown,
-         maleAgeDiff = male - male_AgeKnown) %>% 
+         maleAgeDiff = male - male_AgeKnown) %>%
   summarise(across(c(femaleAgeDiff, maleAgeDiff), ~summary(.))) # OK (all 0, age categories add up to sex category)
 
 # for age and sex or only available: do male/female add up to total count?
-t.sexTotalCheck <- dem_longMissing %>% 
-  filter(missing == "none" | missing == "age") %>% 
-  mutate(sexKnown = rowSums(select(., female, male), na.rm = T)) %>% 
-  mutate(sexDiff = total - sexKnown) %>% 
+t.sexTotalCheck <- dem_longMissing %>%
+  filter(missing == "none" | missing == "age") %>%
+  mutate(sexKnown = rowSums(select(., female, male), na.rm = T)) %>%
+  mutate(sexDiff = total - sexKnown) %>%
   summarise(sexDiff = summary(sexDiff)) # OK (all 0, male/female add up to total)
 
 
 
-## check headline totals against 2021 Global Trends and refugee data finder 
+## check headline totals against 2021 Global Trends and refugee data finder
 t.total <- dem_longMissing %>% # ref finder: 25,733,717
   summarise(total = sum(total)) # OK
 
@@ -474,18 +474,18 @@ t.totalRegion <- dem_longMissing %>% # checked against table 1 in GT 2021
 
 
 ## check missingness proportion
-t.misProp <- dem_longMissing %>% 
-  group_by(missing) %>% 
-  summarise(total = sum(total)) %>% 
-  mutate(prop = total/sum(total)) 
+t.misProp <- dem_longMissing %>%
+  group_by(missing) %>%
+  summarise(total = sum(total)) %>%
+  mutate(prop = total/sum(total))
 
 t.popType.misProp <- dem_longMissing %>% # GT 2021: "For example, demographic data by age and sex is available for 84 per cent of refugees and 42 per cent of Venezuelans displaced abroad at the end of 2021"
-  group_by(popType, missing) %>% 
-  summarise(total = sum(total)) %>% 
+  group_by(popType, missing) %>%
+  summarise(total = sum(total)) %>%
   mutate(prop = total/sum(total)) # OK, same as GT
 
 
-##### VIII. Save dataset in data folder ##### 
+##### VIII. Save dataset in data folder #####
 
 save(dem_longMissing, m49hcr, file = "data/dem_refvda_end2021.RData")
 
